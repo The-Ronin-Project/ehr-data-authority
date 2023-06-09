bec82318c98d5351350db138a16fede6266b60c1
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-06-09 12:19:17 -0500

    INT-1841: Use new server publish and version plugins (#84)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
-    id("com.projectronin.interop.gradle.base")
-    id("ehrda-publish") apply false
-    id("ehrda-version")
+    id("com.projectronin.interop.gradle.server-publish") apply false
+    id("com.projectronin.interop.gradle.server-version")
 }
 
 subprojects {
     apply(plugin = "com.projectronin.interop.gradle.base")
 
     if (project.name != "ehr-data-authority-server") {
-        apply(plugin = "ehrda-publish")
+        apply(plugin = "com.projectronin.interop.gradle.server-publish")
     }
 
     // Disable releases hub from running on the subprojects. Main project will handle it all.
