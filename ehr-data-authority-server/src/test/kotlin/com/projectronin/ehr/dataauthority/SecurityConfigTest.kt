37944d28058dc1e3df05239d99317000c3276518
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-12-11 13:35:39 -0600

    INT-2405: Upgrade to JDK 17 (#173)

--- ehr-data-authority-server/src/test/kotlin/com/projectronin/ehr/dataauthority/SecurityConfigTest.kt
+++ ehr-data-authority-server/src/test/kotlin/com/projectronin/ehr/dataauthority/SecurityConfigTest.kt
@@ -8,10 +8,8 @@ import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.jwt.JwtDecoders
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
-import org.springframework.security.web.DefaultSecurityFilterChain
 
 class SecurityConfigTest {
     @Test
@@ -32,42 +30,6 @@ class SecurityConfigTest {
 
         val jwtDecoder = securityConfig.jwtDecoder()
         assertEquals(mockJwtDecoder, jwtDecoder)
-
-        unmockkAll()
-    }
-
-    @Test
-    fun `builds SecurityFilterChain`() {
-        mockkStatic(JwtDecoders::class)
-        val mockJwtDecoder =
-            mockk<NimbusJwtDecoder> {
-                every { setJwtValidator(any()) } just Runs
-            }
-
-        every { JwtDecoders.fromOidcIssuerLocation<NimbusJwtDecoder>("issuer") } returns mockJwtDecoder
-
-        val mockSecurityFilterChain = mockk<DefaultSecurityFilterChain>()
-        val httpSecurity =
-            mockk<HttpSecurity> {
-                every {
-                    authorizeRequests()
-                        .antMatchers("/actuator/**", "/swagger-ui/**", "/v3/api-docs/**")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                        .and().cors()
-                        .and().oauth2ResourceServer().jwt().decoder(any())
-                } returns mockk()
-                every { build() } returns mockSecurityFilterChain
-            }
-
-        val securityConfig =
-            SecurityConfig().apply {
-                audience = "audience"
-                issuer = "issuer"
-            }
-
-        val securityFilterChain = securityConfig.securityFilterChain(httpSecurity)
-        assertEquals(mockSecurityFilterChain, securityFilterChain)
 
         unmockkAll()
     }
